93364dd432f19bbaa47b25378c5059a42df535f3
Matijs van Zuijlen <matijs@matijs.net>
2014-05-19 14:58:29 +0200

    Fix blog permalink regex
    Use the right codes for beginning and end of string, mkay?

--- app/models/spree/blog.rb
+++ app/models/spree/blog.rb
@@ -5,7 +5,7 @@ class Spree::Blog < ActiveRecord::Base
   has_many :categories, :through => :posts, :source => :post_categories, :uniq => true
 
   validates :name, :presence => true
-  validates :permalink, :uniqueness => true, :format => { :with => /A[a-z0-9\-\_\/]+z/i }, :length => { :within => 3..40 }
+  validates :permalink, :uniqueness => true, :format => { :with => /\A[a-z0-9\-\_\/]+\z/i }, :length => { :within => 3..40 }
   validate  :permalink_availablity
 
   before_validation :normalize_permalink
