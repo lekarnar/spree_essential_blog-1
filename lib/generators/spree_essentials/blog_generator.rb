679b334da1d22ba39ec1cb53c4471f0fa8a3eada
James Hilliard <james.hilliard1@gmail.com>
2014-06-19 04:42:41 -0600

    modernize generator

--- lib/generators/spree_essentials/blog_generator.rb
+++ lib/generators/spree_essentials/blog_generator.rb
@@ -6,13 +6,17 @@ module SpreeEssentials
       
       class_option :add_stylesheets, :type => :boolean, :default => true, :banner => "Append spree_essential_blog to store/all.css"
       
-      def copy_migrations
-        rake "spree_essential_blog:install:migrations"
+      def add_stylesheets
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", "*= require store/spree_essential_blog\n*/", :before => /\*\//, :verbose => true
       end
-      
-      def append_stylesheets
-        return unless options[:add_stylesheets]
-        gsub_file "app/assets/stylesheets/store/all.css", "*/", "*= require store/spree_essential_blog\n*/"
+
+      def run_migrations
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
       end
 
     end
