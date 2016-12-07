0ad3ca9ab4678ef1d0fd33096a9c0671c40510c9
Marijana Molnar <marijana@orangeiceberg.com>
2016-12-07 08:56:41 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,51 @@
-# class Spree::PossibleBlog
-#   def matches?(request)
-#     @locales ||= I18n.available_locales.map &:to_s
-#     components = request.path.gsub(/(^\/+)/, "").split('/')
-#     components.shift if @locales.include?(components.first)
-#     return false if components.first =~ Spree::Blog::RESERVED_PATHS
-#     permalink = components.first
-#     blog = Spree::Blog.find_by_permalink!(permalink) rescue nil
-#     blog.present?
-#   end
-# end
+class Spree::PossibleBlog
+  def matches?(request)
+    @locales ||= I18n.available_locales.map &:to_s
+    components = request.path.gsub(/(^\/+)/, "").split('/')
+    components.shift if @locales.include?(components.first)
+    return false if components.first =~ Spree::Blog::RESERVED_PATHS
+    permalink = components.first
+    blog = Spree::Blog.find_by_permalink!(permalink) rescue nil
+    blog.present?
+  end
+end
 
-# Spree::Core::Engine.add_routes do
-#   scope(:module => "blogs") do
-#     namespace :admin do
-#       resources :blogs, :constraints => { :id => /[a-z0-9\-\_\/]{3,}/ }
+Spree::Core::Engine.add_routes do
+  scope(:module => "blogs") do
+    namespace :admin do
+      resources :blogs, :constraints => { :id => /[a-z0-9\-\_\/]{3,}/ }
 
-#       resources :posts do
-#         resources :images, :controller => "post_images" do
-#           collection do
-#             post :update_positions
-#           end
-#         end
-#         resources :products, :controller => "post_products"
-#         resources :categories, :controller => "post_categories"
-#       end
+      resources :posts do
+        resources :images, :controller => "post_images" do
+          collection do
+            post :update_positions
+          end
+        end
+        resources :products, :controller => "post_products"
+        resources :categories, :controller => "post_categories"
+      end
 
-#       resource :disqus_settings
+      resource :disqus_settings
 
-#     end
+    end
 
-#     # PLZ is there a better way to do this?!
-#     constraints Spree::PossibleBlog.new do
-#       constraints(
-#         :year  => /\d{4}/,
-#         :month => /\d{1,2}/,
-#         :day   => /\d{1,2}/
-#       ) do
-#         get ":blog_id/:year(/:month(/:day))" => "posts#index", :as => :post_date
-#         get ":blog_id/:year/:month/:day/:id" => "posts#show",  :as => :full_post
-#       end
-#       get ":blog_id/category/:id"   => "post_categories#show", :as => :post_category, :constraints => { :id => /.*/ }
-#       get ":blog_id/search/:query"  => "posts#search",         :as => :search_posts, :query => /.*/
-#       get ":blog_id/archive"        => "posts#archive",        :as => :archive_posts
-#       get ":blog_id"                => "posts#index",          :as => :blog_posts
-#     end
-#   end
-# end
+    # PLZ is there a better way to do this?!
+    constraints Spree::PossibleBlog.new do
+       constraints(
+       :year  => /\d{4}/,
+       :month => /\d{1,2}/,
+       :day   => /\d{1,2}/
+     ) do
+       get ":blog_id/:year(/:month(/:day))" => "posts#index", :as => :post_date
+       #get ":blog_id/:year/:month/:day/:id" => "posts#show",  :as => :full_post
+     end
+
+
+     get ":blog_id/kategorija/:id"   => "post_categories#show", :as => :post_category, :constraints => { :id => /.*/ }
+     get ":blog_id/iskanje/:query"  => "posts#search",         :as => :search_posts, :query => /.*/
+     get ":blog_id/arhiv"        => "posts#archive",        :as => :archive_posts
+     get ":blog_id/:id"            => "posts#show",           :as => :full_post
+     get ":blog_id"                => "posts#index",          :as => :blog_posts
+     end
+  end
+end
